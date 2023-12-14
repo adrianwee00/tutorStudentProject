836d49a876f5a7eba0a721264c5a548a3d0e4d5e
adrian <adrian.wee00@gmail.com>
2023-12-14 19:56:29 +0900

    added contact page

---
+++ frontend/src/pages/contact.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+import { AiFillHome } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import { NavbarMyPage } from '../components/navbar';
+
+const Contact = () => {
+
+}
+
+export default Contact
